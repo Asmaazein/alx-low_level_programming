443d2c3e94803811b77a50e58b6631da727e307f
Asmaazein <asmaam.zein@gmail.com>
2023-06-16 18:34:39 -0700

    a

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 /**
- * main - entry point
- * description: print numbers of base16 in lowercase
- * return: always 0 (success)
+ * main - Entry point
+ * Description: print numbers of base16 in lowercase
+ * Return: Always 0 (success)
 */
 
 int main(void)
